b7130ed787f9fc049d9786617bfa69e974272d87
sdunic <sdunic@gmail.com>
2022-09-26 16:36:28 +0200

    fix

--- 01_introductionary_problems/problems_1_5.sql
+++ 01_introductionary_problems/problems_1_5.sql
@@ -4,16 +4,18 @@
 --We have a table called Shippers. Return all the fields from all the shippers
 --
 --Hint
---The standard format for a select statement that returns all columns and all rows is “Select * from 
---TableName”
+--The standard format for a select statement that returns all columns and all rows is 
+--Select * from TableName
 
 select * from Northwind.dbo.Shippers
+
+
 
 
 --2. Certain fields from Categories
 --In the Categories table, selecting all the fields using this SQL:
 --Select * from Categories
---…will return 4 columns. We only want to see two columns, CategoryName and Description. 
+--will return 4 columns. We only want to see two columns, CategoryName and Description. 
 --Hint
 --Instead of * in the Select statement, specify the column names with a comma between them
 
@@ -22,8 +24,11 @@ select
 	Description 
 from Northwind.dbo.Categories
 
+
+
+
 --3. Sales Representatives
---We’d like to see just the FirstName, LastName, and HireDate of all the employees with the Title of Sales 
+--We'd like to see just the FirstName, LastName, and HireDate of all the employees with the Title of Sales 
 --Representative. Write a SQL statement that returns only those employees.
 --Hint
 --To filter out only certain rows from a table, use a Where clause. The format for a where clause with a 
@@ -39,11 +44,14 @@ from Northwind.dbo.Employees
 	where Title = 'Sales Representative'
 	-- ili Title like 'Sales%'
 
+
+
+
 --4. Sales Representatives in the United States
---Now we’d like to see the same columns as above, but only for those employees that both have the title of 
+--Now we'd like to see the same columns as above, but only for those employees that both have the title of 
 --Sales Representative, and also are in the United States.
 --Hint
---To apply multiple filters in a where clause, use “and” to separate the filters.
+--To apply multiple filters in a where clause, use ï¿½andï¿½ to separate the filters.
 
 select 
 	FirstName,
@@ -52,11 +60,23 @@ select
 from Northwind.dbo.Employees
 	where Title = 'Sales Representative' and Country = 'USA'
 
+
+
+
 --5. Orders placed by specific EmployeeID
 --Show all the orders placed by a specific employee. The EmployeeID for this Employee (Steven
---Buchanan) is 5.--Hint
---The EmployeeID is an integer field, and not a string field. So, the value “5” does not need to be 
---surrounded by single quotes in the where clause.select	OrderID,	OrderDatefrom Northwind.dbo.Orderswhere EmployeeID = 5
+--Buchanan) is 5.
+--Hint
+--The EmployeeID is an integer field, and not a string field. So, the value ï¿½5ï¿½ does not need to be 
+--surrounded by single quotes in the where clause.
+
+select
+	OrderID,
+	OrderDate
+from Northwind.dbo.Orders
+where EmployeeID = 5
+
+
 
 
 
